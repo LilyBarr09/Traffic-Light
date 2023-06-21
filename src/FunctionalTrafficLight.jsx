4835668c1bc49fbf7f146f6698466f3c5696a0d5
Liliana Barrera <barrera.jimnlil@gmail.com>
2023-06-21 10:52:58 -0400

    Solved Functional Component traffic light exercise

--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+  const [colorIndex, setColorIndex] = useState(0);
+
+  const switchColorIndex = () =>
+    colorIndex !== 0 ? setColorIndex(colorIndex - 1) : setColorIndex(2);
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={colorIndex === 0 ? "circle red" : "circle black"}></div>
+        <div
+          className={colorIndex === 1 ? "circle yellow" : "circle black"}
+        ></div>
+        <div
+          className={colorIndex === 2 ? "circle green" : "circle black"}
+        ></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={switchColorIndex}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
