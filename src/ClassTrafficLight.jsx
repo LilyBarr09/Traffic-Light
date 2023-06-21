ee417291c02fc42348ca16de348e34098a43e797
Liliana Barrera <barrera.jimnlil@gmail.com>
2023-06-21 11:03:33 -0400

    Finished both Class and Functional traffic light components exercise

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -1,17 +1,40 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+  state = {
+    colorIndex: 0,
+  };
+
+  switchColorIndex = () =>
+    this.state.colorIndex !== 0
+      ? this.setState({ colorIndex: this.state.colorIndex - 1 })
+      : this.setState({ colorIndex: 2 });
+
   render() {
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div
+            className={
+              this.state.colorIndex === 0 ? "circle red" : "circle black"
+            }
+          ></div>
+          <div
+            className={
+              this.state.colorIndex === 1 ? "circle yellow" : "circle black"
+            }
+          ></div>
+          <div
+            className={
+              this.state.colorIndex === 2 ? "circle green" : "circle black"
+            }
+          ></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.switchColorIndex}>
+          Next State
+        </button>
       </div>
     );
   }
